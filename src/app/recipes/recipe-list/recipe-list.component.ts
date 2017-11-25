78dcd8e95044feec7f95d8bfd51085ff31d6d4f1
joezini <josephlukeedwards@gmail.com>
2017-11-25 16:53:49 +0000

    Complete components & data binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,13 +9,26 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Cheeseburger', 'Yummy cheeseburger', 'https://upload.wikimedia.org/wikipedia/commons/d/dc/Lounge_Burger_Wiki.jpg'),
-    new Recipe('Quinotto', 'Yeah hot damn quinotto', 'https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/2290/compressed_Leek_and_Pea_Quinotto_without_22V2jpg_593x426.jpg')
+    new Recipe(
+      'Cheeseburger',
+      'Yummy cheeseburger', 
+      'https://upload.wikimedia.org/wikipedia/commons/d/dc/Lounge_Burger_Wiki.jpg', 
+      [new Ingredient('Patty', 2), new Ingredient('Burger', 1), new Ingredient('Cheese slice', 1), new Ingredient('Lettuce', 2), new Ingredient('Tomato', 1)]),
+    new Recipe(
+      'Quinotto', 
+      'Yeah hot damn quinotto', 
+      'https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/2290/compressed_Leek_and_Pea_Quinotto_without_22V2jpg_593x426.jpg', 
+      [new Ingredient('Quinoa', 500), new Ingredient('Stock', 1), new Ingredient('Cheese', 10), new Ingredient('Butternut squash', 1), new Ingredient('Garlic', 4)])
   ];
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectRecipe(recipe: Recipe) {
+    this.selectedRecipe.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @Output() addedIngredient = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onItemAdded() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+    this.addedIngredient.emit(new Ingredient(name, amount));
   }
 
 }
